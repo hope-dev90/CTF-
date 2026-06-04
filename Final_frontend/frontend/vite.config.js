b522c823fcf263878725d60db1868f1f987bbfb0
mutimutuje483 <mutimutujehope90@gmail.com>
2026-06-04 18:20:52 +0200

    swagger

--- Final_frontend/frontend/vite.config.js
+++ Final_frontend/frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Import this
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add this to the plugins array
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+  },
+});
